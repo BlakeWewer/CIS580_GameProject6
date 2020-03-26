2f44d452d00b3afbdd742a62b1ea65ca1e3f2950
Blake Wewer <blake.wewer@yahoo.com>
2020-03-25 20:27:07 -0500

    Added boost ability.

--- MonoGameWindowsStarter/Player.cs
+++ MonoGameWindowsStarter/Player.cs
@@ -22,6 +22,13 @@ namespace MonoGameWindowsStarter
             Idle = 4,
         }
 
+        public enum SpeedState
+        {
+            Normal = 0,
+            Boost = 1,
+            Penalty = 2,
+        }
+
         public enum FightingState
         {
             Idle = 0,
@@ -52,11 +59,18 @@ namespace MonoGameWindowsStarter
 
         const int FRAME_HEIGHT_GAP = 25;
 
+        const float SPEED_DEFAULT = 1;
+        const float SPEED_BOOST = 3;
+        const float SPEED_PENALTY = .3f;
+        const float MAX_STAMINA = 3000;
+
         // Other variables
         Game game;
         Texture2D texture;
         Texture2D powerUpBar;
         Texture2D powerUpBarBack;
+        int powerUpBarSize;
+        TimeSpan powerUpTimer;
         public MovingState moving_state;
         MovingState prev_moving_state;
         public FightingState fighting_state;
@@ -64,14 +78,22 @@ namespace MonoGameWindowsStarter
         int frame;
         public BoundingRectangle Bounds;
         public Vector2 curPosition;
-        TimeSpan powerUpTimer;
         public Bomb bomb;
         public SoundEffect ouchSFX;
         public SoundEffect ouchBombSFX;
         KeyboardState old_keyboard;
         KeyboardState keyboard;
         SpriteFont font;
-        int powerUpBarSize;
+        public float stamina;
+        Texture2D staminaBar;
+        Texture2D staminaBarBack;
+        double staminaDelay = 500;
+        TimeSpan staminaDelayTimer;
+        int staminaBarSize;
+        bool drainedStaminaPenalty = false;
+        float speedVar = SPEED_DEFAULT;
+        public SpeedState speed_state;
+        
 
         /// <summary>
         /// Creates a new player object
@@ -86,9 +108,11 @@ namespace MonoGameWindowsStarter
             moving_state = MovingState.Idle;
             prev_moving_state = MovingState.Idle;
             fighting_state = FightingState.Idle;
+            speed_state = SpeedState.Normal;
             powerUpTimer = new TimeSpan(0);
             bomb = new Bomb(game);
             keyboard = Keyboard.GetState();
+            stamina = MAX_STAMINA;
         }
 
         /// <summary>
@@ -102,6 +126,8 @@ namespace MonoGameWindowsStarter
             font = game.Content.Load<SpriteFont>("defaultFont");
             powerUpBar = game.Content.Load<Texture2D>("powerUpBarProgress");
             powerUpBarBack = game.Content.Load<Texture2D>("powerUpBarBack");
+            staminaBar = game.Content.Load<Texture2D>("powerUpBarProgress");
+            staminaBarBack = game.Content.Load<Texture2D>("powerUpBarBack");
             bomb.LoadContent();
         }
 
@@ -150,30 +176,64 @@ namespace MonoGameWindowsStarter
 
             bomb.Update(gameTime);
 
+            // Stamina
+            if(stamina < 0)
+            {
+                drainedStaminaPenalty = true;
+                speedVar = SPEED_PENALTY;
+                staminaDelayTimer = new TimeSpan(0);
+                stamina = 0;
+                speed_state = SpeedState.Penalty;
+            }
+            if((keyboard.IsKeyDown(Keys.LeftAlt) || keyboard.IsKeyDown(Keys.RightAlt)) && !drainedStaminaPenalty)
+            {
+                speedVar = SPEED_BOOST;
+                stamina-=(float)gameTime.ElapsedGameTime.TotalMilliseconds;
+                speed_state = SpeedState.Boost;
+            }
+            if (!(keyboard.IsKeyDown(Keys.LeftAlt) || keyboard.IsKeyDown(Keys.RightAlt)) && !drainedStaminaPenalty)
+            {
+                speedVar = SPEED_DEFAULT;
+                stamina += (float)gameTime.ElapsedGameTime.TotalMilliseconds * .9f;
+                speed_state = SpeedState.Normal;
+            }
+            if (drainedStaminaPenalty && !(staminaDelayTimer.TotalMilliseconds < staminaDelay))
+            {
+                stamina += (float)gameTime.ElapsedGameTime.TotalMilliseconds * .4f;
+            }
+            if(stamina >= MAX_STAMINA)
+            {
+                drainedStaminaPenalty = false;
+                stamina = MAX_STAMINA;
+            }
+            staminaDelayTimer += gameTime.ElapsedGameTime;
+            if (stamina == MAX_STAMINA) staminaBarSize = 100;
+            else staminaBarSize = (int)(100 * (stamina / MAX_STAMINA));
+
             // Update the player state based on input
             if (keyboard.IsKeyDown(Keys.Up))
             {
                 prev_moving_state = moving_state;
                 moving_state = MovingState.North;
-                curPosition.Y -= delta * PLAYER_SPEED;
+                curPosition.Y -= delta * PLAYER_SPEED * speedVar;
             }
             else if (keyboard.IsKeyDown(Keys.Left))
             {
                 prev_moving_state = moving_state;
                 moving_state = MovingState.West;
-                curPosition.X -= delta * PLAYER_SPEED;
+                curPosition.X -= delta * PLAYER_SPEED * speedVar;
             }
             else if (keyboard.IsKeyDown(Keys.Right))
             {
                 prev_moving_state = moving_state;
                 moving_state = MovingState.East;
-                curPosition.X += delta * PLAYER_SPEED;
+                curPosition.X += delta * PLAYER_SPEED * speedVar;
             }
             else if (keyboard.IsKeyDown(Keys.Down))
             {
                 prev_moving_state = moving_state;
                 moving_state = MovingState.South;
-                curPosition.Y += delta * PLAYER_SPEED;
+                curPosition.Y += delta * PLAYER_SPEED * speedVar;
             }
             else
             {
@@ -240,10 +300,17 @@ namespace MonoGameWindowsStarter
             }
             bomb.Draw(spriteBatch);
 
-            spriteBatch.DrawString(font, "Power Up: ", new Vector2(200, 0), Color.DarkRed);
+            spriteBatch.DrawString(font, "Bomb Power: ", new Vector2(200, 0), Color.DarkRed);
             spriteBatch.Draw(powerUpBarBack, new Rectangle(300, 0, 100, 20), null, Color.Black, 0, Vector2.Zero, SpriteEffects.None, 1);
             spriteBatch.Draw(powerUpBar, new Rectangle(300, 5, powerUpBarSize, 10), null, Color.Red, 0, Vector2.Zero, SpriteEffects.None, 0);
- //           spriteBatch.DrawString(font, $"{powerUpBarSize}", new Vector2(500, 10), Color.Black);
+
+            spriteBatch.DrawString(font, "Stamina: ", new Vector2(500, 0), Color.DarkBlue);
+            spriteBatch.Draw(staminaBarBack, new Rectangle(600, 0, 100, 20), null, Color.Black, 0, Vector2.Zero, SpriteEffects.None, 1);
+            spriteBatch.Draw(staminaBar, new Rectangle(600, 5, staminaBarSize, 10), null, Color.LightBlue, 0, Vector2.Zero, SpriteEffects.None, 0);
+            spriteBatch.DrawString(font, $"{stamina}", new Vector2(625, 0), Color.White);
+            spriteBatch.DrawString(font, $"{staminaDelayTimer.TotalMilliseconds}", new Vector2(700, 0), Color.White);
+            spriteBatch.DrawString(font, $"{drainedStaminaPenalty && !(staminaDelayTimer.TotalMilliseconds < staminaDelay)}", new Vector2(700, 20), Color.White);
+            //           spriteBatch.DrawString(font, $"{powerUpBarSize}", new Vector2(500, 10), Color.Black);
 
             //spriteBatch.DrawString(
             //    font,
